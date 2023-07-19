14255e8fcff8c394084d35dbd975c193e71cced5
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-19 18:48:07 +0200

    Sets routes

---
+++ src/Pages/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import MovieDetails from 'Pages/MovieDetails/MovieDetails';
+
+const Movies = () => {
+  return (
+    <>
+      <div>Movies Page</div>
+      <MovieDetails />
+    </>
+  );
+};
+
+export default Movies;
